28bd1aa8bc78f7c153a689760a4608267c38e050
Swanand Deshpande <swananddesh15@gmail.com>
2019-03-28 00:38:38 +0530

    Minor changes.

--- app/src/main/java/com/assignment/whatstheweather/DashboardActivity.java
+++ app/src/main/java/com/assignment/whatstheweather/DashboardActivity.java
@@ -1,17 +1,13 @@
 package com.assignment.whatstheweather;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-
 import com.assignment.whatstheweather.utils.Constants;
 import com.assignment.whatstheweather.utils.ServerComm;
 import com.assignment.whatstheweather.utils.Utils;
-
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.util.Iterator;
 
 public class DashboardActivity extends AppCompatActivity {
@@ -36,14 +32,13 @@ public class DashboardActivity extends AppCompatActivity {
 
     public String getQueryString(JSONObject unparsedJson) throws JSONException {
         StringBuilder sb = new StringBuilder();
-        JSONObject json = unparsedJson;
-        Iterator<String> keys = json.keys();
+        Iterator<String> keys = unparsedJson.keys();
         sb.append("?"); //start of query args
         while (keys.hasNext()) {
             String key = keys.next();
             sb.append(key);
             sb.append("=");
-            sb.append(json.get(key));
+            sb.append(unparsedJson.get(key));
             sb.append("&"); //To allow for another argument.
         }
 
@@ -59,8 +54,16 @@ public class DashboardActivity extends AppCompatActivity {
                 @Override
                 public void onSuccess(String result) {
                     try {
-                       JSONObject loginResponseJson = new JSONObject(result);
                         Log.e("Resp Weather :: ",""+result);
+
+                       JSONObject weatherResponseObject = new JSONObject(result);
+                       JSONObject weatherInfo = weatherResponseObject.getJSONObject("main");
+
+                       Double minTemp = (Double) weatherInfo.get("temp_min");
+                       Double maxTemp = (Double) weatherInfo.get("temp_max");
+
+                       Log.e("Temperatures ", "Min : " + minTemp + " Max : " + maxTemp);
+
                     } catch (JSONException e) {
                         e.printStackTrace();
                     }
@@ -68,7 +71,7 @@ public class DashboardActivity extends AppCompatActivity {
                 }
 
                 @Override
-                public void onError(String result, int errorCode) throws JSONException {
+                public void onError(String result, int errorCode) {
                     Utils.showApiErrorMessage(DashboardActivity.this, getString(R.string.error), "");
                 }
             }, DashboardActivity.this).communicateWithServerJsonParameters(Constants.REQUEST_TYPE_GET, "data/2.5/weather"+ requestParams, null, true, true, false, getResources().getString(R.string.please_wait));
