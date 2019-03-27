0f599eff6367f810cce1083e94fb01fe00124fd5
Swanand Deshpande <swananddesh15@gmail.com>
2019-03-28 00:20:05 +0530

    Weather Api test integration.

--- app/src/main/java/com/assignment/whatstheweather/DashboardActivity.java
+++ app/src/main/java/com/assignment/whatstheweather/DashboardActivity.java
@@ -3,6 +3,7 @@ package com.assignment.whatstheweather;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import com.assignment.whatstheweather.utils.Constants;
 import com.assignment.whatstheweather.utils.ServerComm;
@@ -11,29 +12,55 @@ import com.assignment.whatstheweather.utils.Utils;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.Iterator;
+
 public class DashboardActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.put("lat", 35);
+            jsonObject.put("lon", 139);
+            jsonObject.put("appid", Constants.TOKEN);
+            jsonObject.put("units", "metric");
+            String queryString = getQueryString(jsonObject);
+            authenticateUser(queryString);
+        } catch (JSONException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public String getQueryString(JSONObject unparsedJson) throws JSONException {
+        StringBuilder sb = new StringBuilder();
+        JSONObject json = unparsedJson;
+        Iterator<String> keys = json.keys();
+        sb.append("?"); //start of query args
+        while (keys.hasNext()) {
+            String key = keys.next();
+            sb.append(key);
+            sb.append("=");
+            sb.append(json.get(key));
+            sb.append("&"); //To allow for another argument.
+        }
+
+        return sb.toString();
     }
 
     /**
      * Api which will check the user is already present or not
      */
-    private void authenticateUser(final JSONObject requestParams) {
+    private void authenticateUser(final String requestParams) {
         try {
             new ServerComm(new ServerComm.ServerCallback() {
                 @Override
                 public void onSuccess(String result) {
                     try {
-                        JSONObject loginResponseJson = new JSONObject(result);
-                        String userName = loginResponseJson.getString("username");
-                        Constants.AuthConstants.ACCESS_TOKEN = loginResponseJson.getString("access_token");
-
-                        startActivity(new Intent(LoginActivity.this, ContainerActivity.class));
-                        finish();
+                       JSONObject loginResponseJson = new JSONObject(result);
+                        Log.e("Resp Weather :: ",""+result);
                     } catch (JSONException e) {
                         e.printStackTrace();
                     }
@@ -42,9 +69,9 @@ public class DashboardActivity extends AppCompatActivity {
 
                 @Override
                 public void onError(String result, int errorCode) throws JSONException {
-                    Utils.showApiErrorMessage(LoginActivity.this, getString(R.string.error), getString(R.string.invalid_login_crediential));
+                    Utils.showApiErrorMessage(DashboardActivity.this, getString(R.string.error), "");
                 }
-            }, LoginActivity.this).communicateWithServerJsonParameters(Constants.ServerUrl.REQUEST_TYPE_POST, Constants.ServerUrl.LOGIN_USER, requestParams, true, true, false, getResources().getString(R.string.please_wait));
+            }, DashboardActivity.this).communicateWithServerJsonParameters(Constants.REQUEST_TYPE_GET, "data/2.5/weather"+ requestParams, null, true, true, false, getResources().getString(R.string.please_wait));
 
         } catch (Exception ex) {
             ex.printStackTrace();
--- app/src/main/java/com/assignment/whatstheweather/utils/Constants.java
+++ app/src/main/java/com/assignment/whatstheweather/utils/Constants.java
@@ -1,13 +1,17 @@
 package com.assignment.whatstheweather.utils;
 
+import com.android.volley.Request;
+
 public class Constants {
     public static final String BASE_URL = "https://api.openweathermap.org/";
 
-    public static final String TOKEN = "9e2c1050f07c91a00deeea4ba5c647a7a";
+    public static final String TOKEN = "7435a163dd59017aa5478f7379e0a7d9";
 
     public static final int MAX_RETRY_POLICY_VOLLEY = 60000;
 
     public static final class ServerUrl {
         public static final String LOGIN_USER= "";
     }
+    public static final int REQUEST_TYPE_POST = Request.Method.POST;
+    public static final int REQUEST_TYPE_GET = Request.Method.GET;
 }
